8c32c5e152814ac6527d8a18d66b662c39c9e83b
Prince Kakoria <prince.13531@stu.upes.ac.in>
2025-04-23 23:26:39 +0530

    Add files via upload
    inserting node at the beginning of linked list

---
+++ linked_list1.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, data;
+
+    printf("Enter number of nodes to insert at beginning: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+        insertAtBeginning(&head, data);
+    }
+
+    printf("Linked List after insertions: ");
+    displayList(head);
+
+    return 0;
+}
