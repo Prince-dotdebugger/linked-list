5ae5f96e9b97ece862db73964a4ebadc7f15714d
Prince Kakoria <prince.13531@stu.upes.ac.in>
2025-04-23 23:29:13 +0530

    Add files via upload
    inserting node at the end of Linked list

---
+++ linked_list2.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, data;
+
+    printf("Enter number of nodes to insert at end: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+        insertAtEnd(&head, data);
+    }
+
+    printf("Linked List after insertions: ");
+    displayList(head);
+
+    return 0;
+}
